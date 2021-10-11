f1fce213262590a73ba04178aba794f6b9e522b0
RohitBh-GT <67046112+RohitBh-GT@users.noreply.github.com>
2021-10-11 12:00:25 +0530

    .

--- server/controllers/shoes.js
+++ server/controllers/shoes.js
@@ -25,7 +25,7 @@ export const addReview = async(req,res) => {
         if(!(mongoose.Types.ObjectId.isValid(id))){
             return ( res.status(404).send('No such product exists.'));
         }
-        const updatedProduct = await Shoes.findOneAndUpdate({_id:id},{"$push":{"productReviews":review}},{ 'new': true });
+        const updatedProduct = await Shoes.findOneAndUpdate({_id:id},{"$push":{"productReviews":review}},{new: true});
         return res.status(201).json({id,review});
     } catch (error) {
        return res.status(404).json({message:'Server problem.Not able to add review.'});  
